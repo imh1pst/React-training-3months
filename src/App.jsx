02f45f96970f3fcfc5683009d8ab4a0f0ce4247e
A. Matthias Duarte <im.h1pst@gmail.com>
2022-09-28 22:07:13 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+//En react los componentes se definen a travez de funciones  
+//Primer componente:
+function App() {
+
+  return ( //Deben retornar elementos u otros componentes
+    <div className="App"> 
+      <h1>Hola Mundo</h1>
+      <h2>Hola React</h2>
+      <p>Hola Alan Matthias</p>
+    </div>
+  )
+}
+
+export default App
